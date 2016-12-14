1c7cafbdc3a47134ebf0216ed7cca7c3756eee3f
mariateresachaves <teresachavesrpg@gmail.com>
2016-12-14 11:16:37 +0000

    get ip by host name done - no segmentation fault now!

--- Projeto-2/src/clientTCP.c
+++ Projeto-2/src/clientTCP.c
@@ -10,11 +10,12 @@
 #include <signal.h>
 #include <netdb.h>
 #include <string.h>
+#include <errno.h>
 #include "parser.h"
 
 #define SERVER_PORT 21
 
-char* SERVER_ADDR ;
+struct hostent *h;
 
 /**
  * @brief Funcao que verifica os argumentos passados pelo utilizador.
@@ -28,8 +29,26 @@ void check_args(int argc, char ** argv) {
 	}
 }
 
-int main(int argc, char** argv){
+/*	struct hostent {
+			char    *h_name;	Official name of the host.
+			char    **h_aliases;	A NULL-terminated array of alternate names for the host.
+			int     h_addrtype;	The type of address being returned; usually AF_INET.
+			int     h_length;	The length of the address in bytes.
+			char    **h_addr_list;	A zero-terminated array of network addresses for the host.
+};*/
+int get_address() {
+			if ((h=gethostbyname("localhost")) == NULL) {
+					herror("gethostbyname");
+					exit(1);
+			}
 
+			printf("Host name  : %s\n", h->h_name);
+			printf("IP Address : %s\n",inet_ntoa(*((struct in_addr *)h->h_addr)));
+
+			return 0;
+}
+
+int main(int argc, char** argv) {
 	char *buf1;
 	char *buf2;
 	char *buf3;
@@ -39,46 +58,45 @@ int main(int argc, char** argv){
 	struct	sockaddr_in server_addr;
 	int	bytes;
 
+	// verifica se os argumentos
 	check_args(argc,argv);
 
+	// caso nao seja passado um url
 	if (strcmp(argv[2],"default") == 0) {
 		buf1 = "miguel-teresa\n";
 		buf2 = "rcombuefixe\n";
 		buf3 = "192.168.50.111\n";
-		buf4 = "file.txt"; 
-	} else {
+		buf4 = "file.txt";
+	}
+	// caso tenha um url
+	else {
 		parser(argv[2]);
 	}
+
+	// obter o endereço
+	get_address();
 
 	/*server address handling*/
 	bzero((char*)&server_addr,sizeof(server_addr));
 	server_addr.sin_family = AF_INET;
-	//gethostbyname - retorna o endereço ip do server
-	server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);	/*32 bit Internet address network byte ordered*/
+	server_addr.sin_addr.s_addr = inet_addr(h->h_name);	/*32 bit Internet address network byte ordered*/
 	server_addr.sin_port = htons(SERVER_PORT);		/*server TCP port must be network byte ordered */
-    
+
 	/*open an TCP socket*/
 	if ((sockfd = socket(AF_INET,SOCK_STREAM,0)) < 0) {
     		perror("socket()");
         	exit(0);
     	}
 	/*connect to the server*/
-    	if(connect(sockfd, 
-	           (struct sockaddr *)&server_addr, 
+    	if(connect(sockfd,
+	           (struct sockaddr *)&server_addr,
 		   sizeof(server_addr)) < 0){
         	perror("connect()");
 		exit(0);
 	}
-
-	printf("%s\n",SERVER_ADDR);
     	/*send a string to the server*/
-	//bytes = write(sockfd, buf1, strlen(buf1));
+	//bytes = write(sockfd, buf, strlen(buf));
 	//printf("Bytes escritos %d\n", bytes);
 
-	//read string from user
-
 	close(sockfd);
-	exit(0);
 }
-
-
