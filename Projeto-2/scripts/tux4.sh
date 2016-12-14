90ff514671b5b3cefc46c88e7b24dd950a96cac3
mariateresachaves <teresachavesrpg@gmail.com>
2016-12-14 17:53:53 +0000

    fix to scripts

--- Projeto-2/scripts/tux4.sh
+++ Projeto-2/scripts/tux4.sh
@@ -3,5 +3,6 @@ ifconfig eth0 down
 ifconfig eth1 down
 ifconfig eth0 up 172.16.30.254/24
 ifconfig eth1 up 172.16.31.253/24
+route add default gw 172.16.31.254
 echo 1 > /proc/sys/net/ipv4/ip_forward
 echo 0 > /proc/sys/net/ipv4/icmp_echo_ignore_broadcasts
