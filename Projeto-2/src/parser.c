1dca84c49fb3085af0b7ac7eea9dadf90052c3eb
mariateresachaves <teresachavesrpg@gmail.com>
2016-12-14 13:44:00 +0000

    regex done with an example hardcoded

--- Projeto-2/src/parser.c
+++ Projeto-2/src/parser.c
@@ -9,18 +9,19 @@ void parser(char* url, char** user, char** password, char** host, char** url_pat
 	/* Compila a expressao regular */
 	/* ftp://[<user>:<password>@]<host>/<url-path>
 			<user> -> [[A-Za-z0-9_-]+
-			<password> -> [A-Za-z0-9]+ //TODO: mudar regex
-			<host> -> [0-9]+ //TODO: mudar regex
-			<url-path> -> [A-Za-z0-9_-]+ //TODO: mudar regex
+			<password> -> [A-Za-z0-9]+ //TODO: regex apenas aceita passwords com letras numeros _ e -
+			<host> -> [0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}
+			<url-path> -> ([A-Za-z0-9_-]+\\/)+
 	*/
-	reti = regcomp(&regex, "ftp://\\[[A-Za-z0-9_-]+:[A-Za-z0-9]+\\]@[0-9]+\\/[A-Za-z0-9_-]+", REG_EXTENDED);
+	reti = regcomp(&regex, "ftp://\\[[A-Za-z0-9_-]+:[A-Za-z0-9]+\\]@[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\/([A-Za-z0-9_-]+\\/)+", REG_EXTENDED);
 	if (reti) {
 	    fprintf(stderr, "Could not compile regex\n");
 	    exit(1);
 	}
 
 	/* Executa a expressao regular */
-	reti = regexec(&regex, "ftp://[miguel-teresa_6:pass123]@123/url-path_name", 0, NULL, 0);
+	// exemplo de regex a funcionar
+	reti = regexec(&regex, "ftp://[miguel-teresa_6:pass123]@172.16.31.1/url-path/name_rcom", 0, NULL, 0);
 	if (!reti) {
 	    puts("Match");
 	}
